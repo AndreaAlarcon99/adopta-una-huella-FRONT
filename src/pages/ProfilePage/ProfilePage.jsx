039653776b45dc91d6bc0cd65aa0c91b5da50dbb
Nahuel Cimini <nahuel.cimini@gmail.com>
2022-11-04 09:29:27 +0100

    push dependencias ok

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -1,9 +1,25 @@
 import "./ProfilePage.css";
+import userService from "../../services/user.service";
+import animalService from "../../services/animal.service";
+import { useState, useEffect } from 'react'
 
-function ProfilePage() {
+function ProfilePage({user}) {
+
+  const { _id, username, email, imgUser, description, logoUser, } = user
+
+  const [enAdopcion, setEnAdopcion] = useState([]);
+
+  useEffect(() => {
+    animalService.getAnimals(animal.creator === _id)
+    .then(results => setEnAdopcion(results.data))
+    .catch(err => next(err))
+  }, [])
+  
+
+
   return (
-    <div>
-      <h1>Profile page</h1>
+    <div className='shadow-lg p-3 mb-5 bg-body rounded'>
+      
     </div>
   );
 }
