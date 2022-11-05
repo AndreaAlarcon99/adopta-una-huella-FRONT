2fe02569c6268bfdc7a634e4eb2db772b418a8f0
Nahuel Cimini <nahuel.cimini@gmail.com>
2022-11-05 11:22:51 +0100

    toMakePull

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -4,22 +4,33 @@ import { useState, useEffect } from 'react'
 
 function ProfilePage({user}) {
 
-  const { _id, username, email, imgUser, description, logoUser, } = user
+  // const { _id, username, email, imgUser, description, logoUser } = user
 
-  const [enAdopcion, setEnAdopcion] = useState([]);
+  // const [enAdopcion, setEnAdopcion] = useState([]);
 
-  useEffect(() => {
-    animalService.getAnimals(animal.creator === _id)
-    .then(results => setEnAdopcion(results.data))
-    .catch(err => next(err))
-  }, [])
+  // useEffect(() => {
+  //   animalService.getAnimals(_id)
+  //   .then(results => setEnAdopcion(results.data))
+  //   .catch(err => console.log(err))
+  // }, [])
   
 
 
   return (
-    <div className='shadow-lg p-3 mb-5 bg-body rounded'>
-      <h1>patata</h1>
+    <div className="card mb-3" style="max-width: 540px;">
+  <div className="row g-0">
+    <div className="col-md-4">
+      {/* <img src="..." className="img-fluid rounded-start" alt="..."> */}
     </div>
+    <div className="col-md-8">
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p className="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+  </div>
+</div>
   );
 }
 
