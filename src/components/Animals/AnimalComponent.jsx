7b18c57c886f0f4ff1f21bf4684c75eb78de7d9b
Leticia Santos <leticia.s.poveda@gmail.com>
2022-11-07 16:05:24 +0100

    cambios responsive

--- src/components/Animals/AnimalComponent.jsx
+++ src/components/Animals/AnimalComponent.jsx
@@ -3,28 +3,42 @@ import "./AnimalComponent.css";
 
 function AnimalComponent({ animal }) {
   return (
-   
-      <div className="col-7 col-md-3 col-lg-3 p-0" id="cajaAnimal">
-        <img
-          src={animal.imgAnimal}
-          className="card-img-top "
-          alt={animal.animalType}
-        />
-        
-        <div className="card-body">
+    <div className="col-8 col-md-3" id="cajaAnimal">
+      <img
+        src={animal.imgAnimal}
+        className="card-img-top "
+        alt={animal.animalType}
+      />
+
+      <div className="card-body">
         <br></br>
-          <h4 className="card-title">{animal.animalName}</h4>
+        <h4 className="card-title">{animal.animalName}</h4>
 
-          <p className="card-text pt-1 cardDetails">
-            {animal.gender}<br></br>
-            <img className="locationIcon" src="../../locationIcon.png" alt="ubicacion"></img>{animal.location} <br></br>
-            <img className="birthdayIcon" src="../../birthdayIcon.png" alt="nacimiento"></img>{animal.birthday} <br></br>
-            <Link to={"/animales/" + animal._id} className="linkInfo">Más info</Link>
-          </p>
+        <p className="card-text pt-1 cardDetails">
+          {animal.gender}
+          <br></br>
+          <img
+            className="locationIcon"
+            src="../../locationIcon.png"
+            alt="ubicacion"
+          ></img>
+          {animal.location} <br></br>
+          <img
+            className="birthdayIcon"
+            src="../../birthdayIcon.png"
+            alt="nacimiento"
+          ></img>
+          {animal.birthday} <br></br>
+        </p>
+        <Link to={"/animales/" + animal._id} className="linkInfo">
+        <div class="card-footer text-muted" id="footerCard">
+          
+            <span className="text-white text-bold">Más info</span>
+          
         </div>
-
+        </Link>
       </div>
-   
+    </div>
   );
 }
 
