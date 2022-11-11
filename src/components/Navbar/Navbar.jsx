6b855f26644abd64be816e6acdc76ca7c57303e4
Leticia Santos <leticia.s.poveda@gmail.com>
2022-11-11 16:16:40 +0100

    pre pull

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -29,7 +29,7 @@ function Navbar() {
         >
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse " id="navbarTogglerDemo02">
+        <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0 ">
             <li className="nav-item">
               <Link to="/" className="nav-link active" aria-current="page">
